ea744f978c41ee38149b160fc18da13ae79a7301
Sean Chen <seanchen1073@gmail.com>
2024-08-17 16:42:17 +0800

    moved bank_data.json to backend\bank

--- backend/convert_csv_to_json.py
+++ backend/convert_csv_to_json.py
@@ -5,7 +5,7 @@ import json
 csv_file_path = r"C:\Users\Joe\BankSearchData\banks.csv"
 
 # 定義輸出的 JSON 文件路徑
-json_file_path = r"C:\Users\Joe\BankSearch\frontend\src\bank_data.json"
+json_file_path = r"C:\Users\Joe\BankSearch\backend\bank\bank_data.json"
 
 # 打開 CSV 文件並讀取
 with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
