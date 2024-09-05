18911faa1dc6187a3e45f55aba3374174abc00ff
Sean Chen <seanchen1073@gmail.com>
2024-09-05 20:18:04 +0800

    added a feature that can close the dropdown if the user clicks outside of the input

--- frontend/src/components/BankingForm.jsx
+++ frontend/src/components/BankingForm.jsx
@@ -1,34 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import BankNameSection from "./BankNameSection";
 import BranchNameSection from "./BranchNameSection";
 
 const BankingForm = ({ handleBankSearch, handleBranchSearch, filteredBanks, filteredBranches, selectedBank, setSelectedBank }) => {
-    const [activeDropdown, setActiveDropdown] = useState(null);
+  const [activeDropdown, setActiveDropdown] = useState(null);
+  const formRef = useRef(null);
 
-    const handleBankSelect = (bank) => {
-        setSelectedBank(bank);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (formRef.current && !formRef.current.contains(event.target)) {
         setActiveDropdown(null);
+      }
     };
 
-    return (
-        <div className="flex flex-wrap justify-center">
-        <BankNameSection
-            handleSearch={handleBankSearch}
-            filteredBanks={filteredBanks}
-            selectedBank={selectedBank}
-            setSelectedBank={handleBankSelect}
-            isDropdownActive={activeDropdown === "bank"}
-            setActiveDropdown={setActiveDropdown}
-        />
-        <BranchNameSection
-            selectedBank={selectedBank}
-            handleSearch={handleBranchSearch}
-            filteredBranches={filteredBranches}
-            isDropdownActive={activeDropdown === "branch"}
-            setActiveDropdown={setActiveDropdown}
-        />
-        </div>
-    );
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleBankSelect = (bank) => {
+    setSelectedBank(bank);
+    setActiveDropdown(null);
+  };
+
+  const handleDropdownToggle = (dropdownName) => {
+    setActiveDropdown((prevDropdown) => (prevDropdown === dropdownName ? null : dropdownName));
+  };
+
+  return (
+    <div className="flex flex-wrap justify-center" ref={formRef}>
+      <BankNameSection
+        handleSearch={handleBankSearch}
+        filteredBanks={filteredBanks}
+        selectedBank={selectedBank}
+        setSelectedBank={handleBankSelect}
+        isDropdownActive={activeDropdown === "bank"}
+        setActiveDropdown={() => handleDropdownToggle("bank")}
+      />
+      <BranchNameSection
+        selectedBank={selectedBank}
+        handleSearch={handleBranchSearch}
+        filteredBranches={filteredBranches}
+        isDropdownActive={activeDropdown === "branch"}
+        setActiveDropdown={() => handleDropdownToggle("branch")}
+      />
+    </div>
+  );
 };
 
 export default BankingForm;
