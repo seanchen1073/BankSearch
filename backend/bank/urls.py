c858d515092759b34ebba22fa00f7b54fc3db1c6
Sean Chen <seanchen1073@gmail.com>
2024-09-06 19:54:15 +0800

    modified views.py and urls.py to change the route of URL

--- backend/bank/urls.py
+++ backend/bank/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('branch/<str:branch_code>/', views.get_branch_details, name='get_branch_details'),
     path('bank-data/', views.get_bank_data, name='get_bank_data'),
     path('all-bank-data/', views.all_bank_data, name='all_bank_data'),
-    path('bank_data.json', views.get_bank_data, name='get_bank_data_json'),  # 新增這行
+    path('bank_data.json', views.get_bank_data, name='get_bank_data_json'),
+    
+    # 新增的 URL 模式
+    path('<str:bank_code>/<str:branch_code>/', views.get_branch_details, name='branch_details'),
+    path('<str:bank_code>/<str:branch_code>/<str:bank_name>-<str:branch_name>.html', views.branch_detail_page, name='branch_detail_page'),
 ]
