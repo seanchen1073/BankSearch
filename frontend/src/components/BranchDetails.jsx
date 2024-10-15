e069ebd91c282da935cdcf21688b59574a4ee136
Sean <seanchen1073@gmail.com>
2024-10-16 00:48:38 +0800

    modified to open '政府資料開放平台' in a new page instead of changing the current page

--- frontend/src/components/BranchDetails.jsx
+++ frontend/src/components/BranchDetails.jsx
@@ -30,38 +30,38 @@ const BranchDetails = ({ selectedBank, selectedBranch }) => {
 
     return (
         <div className="flex flex-col w-full max-w-[600px] mx-auto">
-        <section className="flex flex-col px-4 py-4 mt-4 border border-gray-700 border-dotted rounded primary bg-green-50">
+            <section className="flex flex-col px-4 py-4 mt-4 border border-gray-700 border-dotted rounded primary bg-green-50">
             <div className="flex flex-col sm:flex-row sm:justify-between">
-            <article className="flex flex-col">
+                <article className="flex flex-col">
                 <h2 className="text-2xl font-bold">{`${bankName} (${bankCode}) ${selectedBranch.name}`}</h2>
                 <p className="mt-2 text-xl">
-                分行代碼：{selectedBranch.code}
-                <button
+                    分行代碼：{selectedBranch.code}
+                    <button
                     onClick={handleCopyBranchCode}
                     className="px-4 py-1 ml-2 text-sm bg-green-500 border border-black rounded hover:bg-green-400 text-green-50 btn"
-                >
+                    >
                     {isCopied ? "已複製" : "複製代碼"}
-                </button>
+                    </button>
                 </p>
                 <p className="mt-2 text-xl">地址：{selectedBranch.address}</p>
                 <p className="mt-2 text-xl">電話：{selectedBranch.tel}</p>
-            </article>
+                </article>
             </div>
             <div className="self-start mt-4 text-green-900 sm:self-end">
-            資料來源：
-            <a className="font-bold text-green-900 gov-link hover:text-blue-700" href="https://data.gov.tw/dataset/6041">
+                資料來源：
+                <a className="font-bold text-green-900 gov-link hover:text-blue-700" href="https://data.gov.tw/dataset/6041" target="_blank">
                 政府資料開放平台
-            </a>
+                </a>
             </div>
-        </section>
-        <section className="mt-2 secondary">
+            </section>
+            <section className="mt-2 secondary">
             <a href="/" className="px-2 py-1 mr-2 text-sm border border-black rounded btn">
-            重新查詢
+                重新查詢
             </a>
             <button className="px-2 py-1 text-sm bg-blue-500 border-black rounded copy-btn hover:bg-blue-400 text-blue-50 btn" onClick={handleCopyLink}>
-            {linkCopied ? "已複製" : "複製此頁面連結"}
+                {linkCopied ? "已複製" : "複製此頁面連結"}
             </button>
-        </section>
+            </section>
         </div>
     );
 };
